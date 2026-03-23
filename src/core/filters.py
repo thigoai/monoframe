89ef3591bcf15456add94fb86e2d711bc21bab0f
thigoai <tfreire352@gmail.com>
2026-03-22 21:14:12 -0300

    add filters with openCV

---
+++ src/core/filters.py
@@ -0,0 +1,16 @@
+import cv2
+
+def limiar_filter(img, intensity):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    _, thresh = cv2.threshold(gray, intensity, 255, cv2.THRESH_BINARY)
+    return thresh
+
+def canny_filter(img, intensity):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+    edges = cv2.Canny(gray, intensity, intensity + 100)
+    return edges
+
+AVAILABLE_FILTERS = {
+    "Limiar": limiar_filter,
+    "Edge (Canny)": canny_filter
+}
