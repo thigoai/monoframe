443fc193c0b1366b1ffbac85b420a5b1d3908e84
thigoai <tfreire352@gmail.com>
2026-03-27 20:53:20 -0300

    add update_frame fucntion

--- src/ui/lens_widget.py
+++ src/ui/lens_widget.py
@@ -30,3 +30,26 @@ class FilterLens(QLabel):
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(30)
+
+    def update_frame(self):
+        sw = QApplication.primaryScreen().size().width()
+        sh = QApplication.primaryScreen().size().height()
+
+        self.setWindowOpacity(0.0)
+        QApplication.processEvents()
+        time.sleep(0.02)
+
+        raw_img = self.capturer.grab(self.x(), self.y(), self.width(), self.height(), sw, sh)
+        self.setWindowOpacity(1.0)
+
+        if raw_img is None:
+            return
+        
+        active_filter_name = self.filter_names[self.current_filter_idx]
+        active_filter_func = AVAILABLE_FILTERS[active_filter_name]
+
+        qimg = self.engine.process(raw_img, active_filter_func, self.parameter, self.delay_ms)
+        if qimg:
+            self.setPixmap(QPixmap.fromImage(qimg))
+
+            
