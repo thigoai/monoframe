40da911b2d53505a0e322580996c64ab016683b8
thigoai <tfreire352@gmail.com>
2026-03-28 18:44:46 -0300

    limiar filter first

--- src/core/filters.py
+++ src/core/filters.py
@@ -1,8 +1,5 @@
 import cv2
 
-def none_filter(img, intensity):
-    return cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
-
 def limiar_filter(img, intensity):
     gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
     _, thresh = cv2.threshold(gray, intensity, 255, cv2.THRESH_BINARY)
@@ -13,8 +10,10 @@ def canny_filter(img, intensity):
     edges = cv2.Canny(gray, intensity, intensity + 100)
     return edges
 
+def none_filter(img, intensity):
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+
 AVAILABLE_FILTERS = {
-    "None": none_filter,
-    "Limiar": limiar_filter
-    #"Edge (Canny)": canny_filter
+    "Limiar": limiar_filter,
+    "None": none_filter
 }
