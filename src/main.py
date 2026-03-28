cacba1ffd227eeef55c58aaa45fa2a07d5a12158
thigoai <tfreire352@gmail.com>
2026-03-27 21:55:44 -0300

    update main

--- src/main.py
+++ src/main.py
@@ -1,41 +1,16 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QVBoxLayout
-from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtGui import QColor
 import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+from ui.lens_widget import FilterLens
 
-class JanelaTransparente(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowFlags(
-            Qt.FramelessWindowHint |  
-            Qt.WindowStaysOnTopHint    
-        )
-        self.setAttribute(Qt.WA_TranslucentBackground) 
-        self.resize(700, 500)
-        self._drag_pos = QPoint()
-        self.init_ui()
+def main():
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    
+    app = QApplication(sys.argv)
+    lens = FilterLens()
+    lens.show()
+    sys.exit(app.exec_())
 
-    def init_ui(self):
-        layout = QVBoxLayout(self)
-        layout.setContentsMargins(0, 0, 0, 0)
-
-    def mousePressEvent(self, e):
-        self._drag_pos = e.globalPos()
-
-    def mouseMoveEvent(self, e):
-        delta = e.globalPos() - self._drag_pos
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self._drag_pos = e.globalPos()
-
-    def paintEvent(self, e):
-        from PyQt5.QtGui import QPainter
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        painter.setBrush(QColor(0, 0, 0, 120)) 
-        painter.setPen(Qt.NoPen)
-        painter.drawRoundedRect(self.rect(), 10, 10)
-
-app = QApplication(sys.argv)
-win = JanelaTransparente()
-win.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    main()
